32091542cbdef0e14be322361cf85f00d28c9cc5
Ên Quân <119003165+katori1307@users.noreply.github.com>
2022-12-15 22:25:41 +0700

    update hàm main

--- main.cpp
+++ main.cpp
@@ -29,51 +29,7 @@
 
 int main()
 {
-	/*SetConsoleTitle(L"Crossy Road");*/
-	/*handle->FixConsoleWindow();
-	handle->hideCursor();*/
-	
-	//g.drawCRGameBoard(5, 4); //Mặc định là (x,y) = (5,4)
-	///*handle->gotoxy(0, 0);
-	//cout << "x";*/
-	/*P.Draw();
-	thread functest1(threadFunc);
-	thread functest2(threadFunc2);
-	functest1.join();
-	functest2.join();*/                   
-
 	Launch();
-
-	/*Console handle;
-	handle.hideCursor();
-	Game g;
-	g.drawCRGameBoard(5, 4);*/
-	
-	/*handle.GotoXY(109, 4);
-	cout << "X";
-	handle.GotoXY(113, 4);
-	cout << "X";
-	handle.GotoXY(113 + 34, 4);
-	cout << "X";*/
-	//system("pause");
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-
-
-
-	
-	
-	
-	
-
 
 	return 0;
 }
